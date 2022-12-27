77d2ae859a0711a0d63a1b4a5f48b89add2158e4
Andrew <andrewnleon@gmail.com>
2022-12-27 00:07:10 -0500

    Clear cache and remove images

--- backend/src/api/employer/controllers/employer.ts
+++
@@ -1,7 +0,0 @@
-/**
- * employer controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::employer.employer');
--- backend/src/api/employer/services/employer.ts
+++
@@ -1,7 +0,0 @@
-/**
- * employer service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::employer.employer');
